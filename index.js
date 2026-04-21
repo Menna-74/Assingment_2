88c04e6c1280f1d88d5e7fed90f1d41c4b3404a8
Menna Ahmed <mennaahmed7420@gmail.com>
2026-04-21 17:09:21 +0200

    init repo

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+app.get("/welcome", (req, res) => {
+  res.send("Welcome to my server !");
+});
+
+app.post("/submit", (req, res) => {
+  console.log("Data from client:", req.body);
+  res.send("Data received");
+});
+
+const users = [
+  { id: 1, name: "John Doe", email: "john@example.com" },
+  { id: 2, name: "Jane Smith", email: "jane@example.com" },
+  { id: 3, name: "Alice Johnson", email: "alice@example.com" },
+];
+
+app.get("/users", (req, res) => {
+    res.status(200);
+    res.json(users);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port:${port}`);
+});
